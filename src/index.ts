13ab67a56e789507313420129628c05390db8cff
Alex Victoor <alexvictoor@gmail.com>
2022-04-04 22:51:00 +0200

    Upgrade AssemblyScript

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-import { demoAdd} from './add';
-import { demoHello } from './hello';
+import { instantiate } from "../build/debug";
 
-demoAdd();
-demoHello();
+const init = async () => {
+    const { add } = await instantiate(await WebAssembly.compileStreaming(globalThis.fetch("debug.wasm")), {});
+    console.log(add(42, 36));
+}
+
+init();
+
+
