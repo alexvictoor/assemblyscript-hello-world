9572e02536147f7a46ef90e8a6fb4d4bce13426e
Alex Victoor <alexvictoor@gmail.com>
2022-04-20 15:55:55 +0200

    Improve wasm bootstrap

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import { instantiate } from "../build/debug";
 
 const init = async () => {
-    const { add, hello } = await instantiate(await WebAssembly.compileStreaming(globalThis.fetch("debug.wasm")), {
-        env: {}
-    });
-    console.log('[wasm] ' + add(42, 36));
-    console.log('[wasm] ' + hello('Devoxx'));
-}
+  
+  const wasmModule = await instantiate(
+    await WebAssembly.compileStreaming(globalThis.fetch("debug.wasm")),
+    {
+      env: {},
+    }
+  );
+
+  const { add, hello } = wasmModule;
+  
+  console.log("[wasm] " + add(42, 36));
+  console.log("[wasm] " + hello("Devoxx"));
+};
 
 init();
-
-
